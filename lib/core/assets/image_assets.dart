3ab9cb446caa66b6ecb1a956a9a9de3bc9b11766
William Blair <w.blair@lancaster.ac.uk>
2023-04-13 03:51:32 -0400

    Move splash logo path to splash constants folder

--- lib/core/assets/image_assets.dart
+++
@@ -1,5 +0,0 @@
-const String kImagePath = 'assets/images';
-
-class ImageAssets {
-  static const String kSplashLogo = '$kImagePath/um_logo.png';
-}
---
+++ lib/features/splash/constants/splash_constants.dart
@@ -0,0 +1,6 @@
+import '../../../core/constants/assets/image_assets.dart';
+
+class SplashConstants {
+  static const String kLogo = '${ImageAssets.kImagePath}/um_logo.png';
+  static const int kDelay = 3;
+}

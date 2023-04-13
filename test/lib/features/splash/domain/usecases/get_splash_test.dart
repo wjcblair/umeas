ad933dc2c5bfd2a500817390c00dd1e8d44d6035
William Blair <w.blair@lancaster.ac.uk>
2023-04-13 03:55:25 -0400

    Test getSplash usecase

--- test/lib/features/splash/domain/usecases/get_splash_test.dart
+++ test/lib/features/splash/domain/usecases/get_splash_test.dart
@@ -1,39 +1,43 @@
 import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:umeas/core/usecases/params.dart';
+import 'package:umeas/core/layer_abstractions/domain/usecases/noparams.dart';
+import 'package:umeas/features/splash/constants/splash_constants.dart';
 import 'package:umeas/features/splash/domain/entities/splash.dart';
-import 'package:umeas/features/splash/domain/repositories/splash_repository.dart';
+import 'package:umeas/features/splash/domain/repositories/i_splash_repository_contract.dart';
 import 'package:umeas/features/splash/domain/usecases/get_splash.dart';
 
-class MockSplashRepository extends Mock implements ISplashRepository {}
+class MockSplashRepository extends Mock implements ISplashRepositoryContract {}
 
 void main() {
   late GetSplash usecase;
   late MockSplashRepository mockSplashRepository;
+  late String tImagePath;
+  late Splash tSplash;
+  late int tDelay;
 
   setUp(() {
     mockSplashRepository = MockSplashRepository();
     usecase = GetSplash(mockSplashRepository);
+    tImagePath = SplashConstants.kLogo;
+    tDelay = SplashConstants.kDelay;
+    tSplash = Splash(delay: tDelay, image: AssetImage(tImagePath));
   });
 
-  const tImagePath = "assets/images/splash.png";
-  const tSplash = Splash(imagePath: tImagePath);
-
   test(
-    "Should get the splash data from the repository",
+    'should get the splash data when the call to the repository is successful',
     () async {
       // Arrange
-      when(() => mockSplashRepository.getSplash(any()))
-          .thenAnswer((_) async => const Right(tSplash));
+      when(() => mockSplashRepository.getSplash())
+          .thenAnswer((_) async => Right(tSplash));
 
       // Act
-      final result =
-          await usecase(const Params(data: {'imagePath': tImagePath}));
+      final result = await usecase(NoParams());
 
       // Assert
-      expect(result, const Right(tSplash));
-      verify(() => mockSplashRepository.getSplash(tImagePath)).called(1);
+      expect(result, Right(tSplash));
+      verify(() => mockSplashRepository.getSplash()).called(1);
       verifyNoMoreInteractions(mockSplashRepository);
     },
   );

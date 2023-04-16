f1eeb44a23c2fc4a002a3f72b9a2c934528dd47c
William Blair <w.blair@lancaster.ac.uk>
2023-04-16 19:36:33 -0400

    Update IFailure to AppFailure in splash repo

--- lib/features/splash/domain/repositories/i_splash_repository_contract.dart
+++ lib/features/splash/domain/repositories/i_splash_repository_contract.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:umeas/features/splash/domain/entities/splash.dart';
 
-import '../../../../core/error/failures/i_failure.dart';
+import '../../../../core/error/failures/app_failure.dart';
 
 abstract class ISplashRepositoryContract {
-  Future<Either<IFailure, Splash>> getSplash();
+  Future<Either<AppFailure, Splash>> getSplash();
 }

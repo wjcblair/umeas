84d0adf6513afbc6d4bd1aa62bceef97d4c60322
William Blair <w.blair@lancaster.ac.uk>
2023-04-18 19:52:05 -0400

    Add BuildContext Localization extension

---
+++ lib/extensions/buildcontext/loc.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart'
+    show AppLocalizations;
+import 'package:flutter/material.dart' show BuildContext;
+
+extension Localization on BuildContext {
+  AppLocalizations get loc => AppLocalizations.of(this)!;
+}

cc3276b4703319c9f9ed6a2aa6700ef485f9f7c3
William Blair <w.blair@lancaster.ac.uk>
2023-04-18 19:58:57 -0400

    Update splash nextRoute to auth

--- lib/features/splash/constants/splash_constants.dart
+++ lib/features/splash/constants/splash_constants.dart
@@ -5,5 +5,5 @@ class SplashConstants {
   static const String kLogo = '${ImageAssets.kImagePath}/um_logo.png';
   static const int kDelay = 3;
   static const int kSplashFailureDuration = 5;
-  static const String kNextRoute = Routes.home;
+  static const String kNextRoute = Routes.auth;
 }

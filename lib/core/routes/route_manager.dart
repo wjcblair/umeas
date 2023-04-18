8d11d39913518bcd920f86345055addc520d1bc6
William Blair <w.blair@lancaster.ac.uk>
2023-04-18 19:58:44 -0400

    Add auth routes

--- lib/core/routes/route_manager.dart
+++ lib/core/routes/route_manager.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:umeas/features/auth/presentation/pages/auth_page.dart';
+import 'package:umeas/features/auth/presentation/pages/forgot_password_page.dart';
+import 'package:umeas/features/auth/presentation/pages/login_page.dart';
+import 'package:umeas/features/auth/presentation/pages/register_page.dart';
+import 'package:umeas/features/auth/presentation/pages/verify_email_page.dart';
 import 'package:umeas/features/home/presentation/pages/home_page.dart';
 
 import '../../features/splash/presentation/pages/splash_page.dart';
@@ -6,6 +11,11 @@ import '../../features/splash/presentation/pages/splash_page.dart';
 class Routes {
   static const String splash = '/';
   static const String home = '/home';
+  static const String auth = '/auth';
+  static const String login = '/auth/login';
+  static const String register = '/auth/register';
+  static const String verifyEmail = '/auth/verifyEmail';
+  static const String forgotPassword = '/auth/forgotPassword';
 }
 
 class RouteGenerator {
@@ -18,7 +28,37 @@ class RouteGenerator {
       case Routes.home:
         return MaterialPageRoute(
           builder: (_) => const HomePage(
-            title: 'Home View',
+            title: 'Home Page',
+            key: null,
+          ),
+        );
+      case Routes.auth:
+        return MaterialPageRoute(
+          builder: (_) => const AuthPage(
+            key: null,
+          ),
+        );
+      case Routes.login:
+        return MaterialPageRoute(
+          builder: (_) => const LoginPage(
+            key: null,
+          ),
+        );
+      case Routes.register:
+        return MaterialPageRoute(
+          builder: (_) => const RegisterPage(
+            key: null,
+          ),
+        );
+      case Routes.verifyEmail:
+        return MaterialPageRoute(
+          builder: (_) => const VerifyEmailPage(
+            key: null,
+          ),
+        );
+      case Routes.forgotPassword:
+        return MaterialPageRoute(
+          builder: (_) => const ForgotPasswordPage(
             key: null,
           ),
         );

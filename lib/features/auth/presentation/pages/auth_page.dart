7ef537abb0ff4eab8614f0aeaf545f8be0d75d06
William Blair <w.blair@lancaster.ac.uk>
2023-04-18 20:01:02 -0400

    Add auth master page

---
+++ lib/features/auth/presentation/pages/auth_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:umeas/features/auth/presentation/pages/forgot_password_page.dart';
+import 'package:umeas/features/auth/presentation/pages/login_page.dart';
+import 'package:umeas/features/auth/presentation/pages/register_page.dart';
+import 'package:umeas/features/auth/presentation/pages/verify_email_page.dart';
+import 'package:umeas/features/home/presentation/pages/home_page.dart';
+
+import '../../../../core/helpers/loading/loading.dart';
+import '../../../../injection_container.dart';
+import '../bloc/auth_bloc.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) {
+        final authBloc = sl<AuthBloc>();
+        authBloc.add(AuthInitializeEvent());
+        return authBloc;
+      },
+      child: BlocConsumer<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state.isLoading) {
+            LoadingScreen().show(
+              context: context,
+              text: state.loadingText ?? 'Please wait a moment',
+            );
+          } else {
+            LoadingScreen().hide();
+          }
+        },
+        builder: (context, state) {
+          if (state is AuthLoggedInState) {
+            return const HomePage(
+              title: 'Home Page',
+            );
+          } else if (state is AuthNeedsVerificationState) {
+            print("auth page recieved verify email state");
+            return const VerifyEmailPage();
+          } else if (state is AuthLoggedOutState) {
+            print("auth page recieved logged out state");
+            return const LoginPage();
+          } else if (state is AuthForgotPasswordState) {
+            print("auth page recieved forgot password state");
+            return const ForgotPasswordPage();
+          } else if (state is AuthRegisteringState) {
+            print("auth page recieved register state");
+            return const RegisterPage();
+          } else {
+            return const Scaffold(
+              body: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}

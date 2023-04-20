ae0333bc19c64191592263162e9eac834d5976f0
William Blair <w.blair@lancaster.ac.uk>
2023-04-20 02:59:48 -0400

    Add email text field

--- lib/features/auth/presentation/widgets/textfields/email_text_field.dart
+++ lib/features/auth/presentation/widgets/textfields/email_text_field.dart
@@ -3,16 +3,40 @@ import 'package:flutter/material.dart';
 import 'auth_text_field.dart';
 
 class EmailTextField extends AuthTextField {
-  const EmailTextField({
+  EmailTextField({
     Key? key,
+    required BuildContext context,
     required TextEditingController controller,
-    required String hintText,
+    required ValueNotifier<bool> isValid,
   }) : super(
           key: key,
           controller: controller,
           keyboardType: TextInputType.emailAddress,
-          hintText: hintText,
+          hintText: 'Email',
+          prefixIcon: const Icon(Icons.email),
           enableSuggestions: false,
           autocorrect: false,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter your email';
+            } else {
+              if (!isEmailValid(value)) {
+                return 'Please enter a valid email';
+              }
+              return null;
+            }
+          },
+          onChanged: (value) {
+            isValid.value = isEmailValid(value);
+          },
+          isValid: isValid,
         );
+
+  static bool isEmailValid(String? email) {
+    if (email == null) return false;
+    String emailRegex =
+        r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+    RegExp regex = RegExp(emailRegex);
+    return regex.hasMatch(email);
+  }
 }

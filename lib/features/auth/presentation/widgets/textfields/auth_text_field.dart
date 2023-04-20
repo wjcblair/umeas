cc544eaa31b4cbf10d7e816a83ebf2e941fa34f5
William Blair <w.blair@lancaster.ac.uk>
2023-04-20 02:59:35 -0400

    Add auth text field base class

--- lib/features/auth/presentation/widgets/textfields/auth_text_field.dart
+++ lib/features/auth/presentation/widgets/textfields/auth_text_field.dart
@@ -4,9 +4,13 @@ class AuthTextField extends StatelessWidget {
   final TextEditingController controller;
   final TextInputType keyboardType;
   final String hintText;
+  final Widget prefixIcon;
   final bool obscureText;
   final bool enableSuggestions;
   final bool autocorrect;
+  final String? Function(String?) validator;
+  final ValueChanged<String>? onChanged;
+  final ValueNotifier<bool> isValid;
 
   const AuthTextField({
     Key? key,
@@ -16,19 +20,27 @@ class AuthTextField extends StatelessWidget {
     this.obscureText = false,
     this.enableSuggestions = false,
     this.autocorrect = false,
+    required this.prefixIcon,
+    required this.validator,
+    required this.onChanged,
+    required this.isValid,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
       controller: controller,
       keyboardType: keyboardType,
       decoration: InputDecoration(
         hintText: hintText,
+        prefixIcon: prefixIcon,
       ),
       obscureText: obscureText,
       enableSuggestions: enableSuggestions,
       autocorrect: autocorrect,
+      validator: validator,
+      onChanged: onChanged,
     );
   }
 }

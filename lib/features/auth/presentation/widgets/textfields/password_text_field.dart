180a5444e6d8b222d93c9e177ed9194c3a59ea4b
William Blair <w.blair@lancaster.ac.uk>
2023-04-20 02:59:57 -0400

    Add password text field

--- lib/features/auth/presentation/widgets/textfields/password_text_field.dart
+++ lib/features/auth/presentation/widgets/textfields/password_text_field.dart
@@ -3,17 +3,39 @@ import 'package:flutter/material.dart';
 import 'auth_text_field.dart';
 
 class PasswordTextField extends AuthTextField {
-  const PasswordTextField({
+  PasswordTextField({
     Key? key,
+    required BuildContext context,
     required TextEditingController controller,
-    required String hintText,
+    required ValueNotifier<bool> isValid,
   }) : super(
           key: key,
           controller: controller,
           keyboardType: TextInputType.visiblePassword,
-          hintText: hintText,
+          hintText: 'Password',
+          prefixIcon: const Icon(Icons.lock),
           enableSuggestions: false,
           autocorrect: false,
           obscureText: true,
+          validator: (value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter your password';
+            } else {
+              if (!isPasswordValid(value)) {
+                return 'Password must be at least 6 characters long';
+              }
+              return null;
+            }
+          },
+          onChanged: (value) {
+            isValid.value = isPasswordValid(value);
+          },
+          isValid: isValid,
         );
+
+  static bool isPasswordValid(String? password) {
+    if (password == null) return false;
+    // TODO: Add some advanced password checks (uppercase/lowercase, at least 1 number, ...)
+    return password.length >= 6;
+  }
 }

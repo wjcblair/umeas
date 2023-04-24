1e38cef7b9b77fae6aaab1f56b1125e5de0b41c2
William Blair <w.blair@lancaster.ac.uk>
2023-04-24 06:41:29 -0400

    Add state opacities

---
+++ lib/core/presentation/resources/state_opacity.dart
@@ -0,0 +1,9 @@
+class StateOpacity {
+  static const double enabled = 1.0;
+  static const double disabled = 0.12;
+  static const double disabledText = 0.38;
+  static const double hovered = 0.08;
+  static const double focused = 0.12;
+  static const double pressed = 0.12;
+  static const double drag = 0.16;
+}

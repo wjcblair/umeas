2129ffbf3d6529212c32d9d7a9294e9adefd072d
William Blair <w.blair@lancaster.ac.uk>
2023-04-24 06:42:32 -0400

    Update image assets

--- lib/core/assets/image_assets.dart
+++ lib/core/assets/image_assets.dart
@@ -1,4 +1,5 @@
 class ImageAssets {
   static const kImagePath = 'assets/images';
   static const kUmeasVerbosePath = '$kImagePath/umeas_verbose.png';
+  static const kUmeasPath = '$kImagePath/umeas.png';
 }

b14a29a24e34578565245517dc279ee081356368
William Blair <w.blair@lancaster.ac.uk>
2023-04-30 17:46:53 -0400

    Make methods private

--- lib/core/constants/screen_constants.dart
+++ lib/core/constants/screen_constants.dart
@@ -7,18 +7,18 @@ class ScreenConstants {
 
   factory ScreenConstants() => instance;
 
-  Size displaySize(BuildContext context) {
+  Size _displaySize(BuildContext context) {
     debugPrint('Size = ${MediaQuery.of(context).size}');
     return MediaQuery.of(context).size;
   }
 
   double displayHeight(BuildContext context) {
-    debugPrint('Height = ${displaySize(context).height}');
-    return displaySize(context).height;
+    debugPrint('Height = ${_displaySize(context).height}');
+    return _displaySize(context).height;
   }
 
   double displayWidth(BuildContext context) {
-    debugPrint('Width = ${displaySize(context).width}');
-    return displaySize(context).width;
+    debugPrint('Width = ${_displaySize(context).width}');
+    return _displaySize(context).width;
   }
 }

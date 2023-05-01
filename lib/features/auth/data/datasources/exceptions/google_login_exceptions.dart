998b1cf373214f9da3c35b31af9e2d8387f65487
William Blair <w.blair@lancaster.ac.uk>
2023-05-01 14:34:57 -0400

    Add google sign in exceptions

--- lib/features/auth/data/datasources/exceptions/google_login_exceptions.dart
+++ lib/features/auth/data/datasources/exceptions/google_login_exceptions.dart
@@ -1,3 +1,7 @@
 class GoogleSignInCancelledException implements Exception {}
 
 class GoogleSignInServerErrorException implements Exception {}
+
+class AccountExistsWithDifferentCredentialAuthException implements Exception {}
+
+class InvalidCredentialAuthException implements Exception {}

53fa15a334dd183a1fd4c31baafc79109bd48b7b
William Blair <w.blair@lancaster.ac.uk>
2023-04-30 23:39:35 -0400

    Add home page

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -1,86 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:umeas/core/extensions/buildcontext/loc.dart';
+import 'package:umeas/core/presentation/widgets/base_page.dart';
 
-import '../../../../config/config_reader.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-import '../../../../core/enums/menu_action.dart';
-import '../../../../core/presentation/widgets/dialogs/logout_dialog.dart';
-import '../../../auth/presentation/bloc/auth/auth_bloc.dart';
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   final String title;
 
   const HomePage({super.key, required this.title});
 
   @override
-  // ignore: library_private_types_in_public_api
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter += ConfigReader.incrementAmount;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        actions: [
-          PopupMenuButton<MenuAction>(
-            onSelected: (value) async {
-              switch (value) {
-                case MenuAction.logout:
-                  final shouldLogout = await showLogOutDialog(context);
-                  if (shouldLogout) {
-                    // ignore: use_build_context_synchronously
-                    context.read<AuthBloc>().add(
-                          AuthLogOutEvent(),
-                        );
-                  }
-              }
-            },
-            itemBuilder: (context) {
-              return [
-                PopupMenuItem<MenuAction>(
-                  value: MenuAction.logout,
-                  child: Text(context.loc.logout_button),
-                ),
-              ];
-            },
-          )
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              AppLocalizations.of(context)!.push_button_text,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              'Revealed secret:\n${ConfigReader.secretKey}',
-              textAlign: TextAlign.center,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return const BasePage(
+        title: 'HOME',
+        child: Center(
+          child: Text('Home Page Content'),
+        ));
   }
 }

9171189310e36102a82f8cc520250d7494ef247b
William Blair <w.blair@lancaster.ac.uk>
2023-05-01 12:43:15 -0400

    Fix mockNetwork required arg squiggly

--- lib/features/inbox/data/repositories/inbox_repository.dart
+++ lib/features/inbox/data/repositories/inbox_repository.dart
@@ -1,4 +1,5 @@
 import 'package:umeas/core/data/exceptions/server_exception.dart';
+import 'package:umeas/core/network/i_network_info_contract.dart';
 import 'package:umeas/features/inbox/data/datasources/local/i_inbox_local_datasource_contract.dart';
 import 'package:umeas/features/inbox/data/datasources/remote/i_inbox_remote_datasource_contract.dart';
 import 'package:umeas/features/inbox/domain/entities/message.dart';
@@ -9,13 +10,12 @@ import 'package:umeas/features/inbox/domain/repositories/i_inbox_repository_cont
 import '../../../../core/data/exceptions/cache_exception.dart';
 import '../../../../core/domain/failures/cache_failure.dart';
 import '../../../../core/domain/failures/server_failure.dart';
-import '../../../../core/network/network_info.dart';
 import '../datasources/exceptions/inbox_exceptions.dart';
 
 class InboxRepository implements IInboxRepositoryContract {
   final IInboxLocalDatasourceContract localDatasource;
   final IInboxRemoteDatasourceContract remoteDatasource;
-  final NetworkInfo networkInfo;
+  final INetworkInfoContract networkInfo;
 
   InboxRepository({
     required this.localDatasource,
--- test/lib/features/inbox/data/repositories/inbox_repository_test.dart
+++ test/lib/features/inbox/data/repositories/inbox_repository_test.dart
@@ -3,7 +3,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:umeas/core/data/exceptions/cache_exception.dart';
 import 'package:umeas/core/data/exceptions/server_exception.dart';
-import 'package:umeas/core/domain/failures/app_failure.dart';
 import 'package:umeas/core/domain/failures/cache_failure.dart';
 import 'package:umeas/core/domain/failures/server_failure.dart';
 import 'package:umeas/core/network/i_network_info_contract.dart';
@@ -11,7 +10,6 @@ import 'package:umeas/features/inbox/data/datasources/local/i_inbox_local_dataso
 import 'package:umeas/features/inbox/data/datasources/remote/i_inbox_remote_datasource_contract.dart';
 import 'package:umeas/features/inbox/data/models/message_model.dart';
 import 'package:umeas/features/inbox/data/repositories/inbox_repository.dart';
-import 'package:umeas/features/inbox/domain/entities/message.dart';
 
 class MockRemoteDataSource extends Mock
     implements IInboxRemoteDatasourceContract {}
@@ -36,81 +34,43 @@ void main() {
       remoteDatasource: mockRemoteDataSource,
       networkInfo: mockNetworkInfo,
     );
-  });
 
-  void runTestsOnline(Function body) {
-    group('device is online', () {
-      setUp(() {
-        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+    void runTestsOnline(Function body) {
+      group('device is online', () {
+        setUp(() {
+          when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
+        });
+        body();
       });
-      body();
-    });
-  }
+    }
 
-  void runTestsOffline(Function body) {
-    group('device is offline', () {
-      setUp(() {
-        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => false);
+    void runTestsOffline(Function body) {
+      group('device is offline', () {
+        setUp(() {
+          when(() => mockNetworkInfo.isConnected)
+              .thenAnswer((_) async => false);
+        });
+        body();
       });
-      body();
-    });
-  }
+    }
 
-  group('getEmails', () {
-    const tSender = 'testSender@test.com';
-    const tSubject = 'testSubject';
-    final tDate = DateTime.now();
-    const tBody = 'testBody';
-    final tEmailModel = MessageModel(
-      sender: tSender,
-      subject: tSubject,
-      date: tDate,
-      body: tBody,
-    );
-    final tEmails = [tEmailModel];
-    test(
-      'should check if the device is online',
-      () async {
-        // Arrange
-        when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
-        when(() => mockRemoteDataSource.fetchMessages())
-            .thenAnswer((_) async => tEmails);
-        when(() => mockLocalDataSource.cacheMessages(any()))
-            // ignore: avoid_returning_null_for_void
-            .thenAnswer((_) async => null);
-
-        // Act
-        await repository.fetchMessages();
-
-        // Assert
-        verify(() => mockNetworkInfo.isConnected);
-      },
-    );
-
-    runTestsOnline(() {
-      test(
-        'should return remote data when the call to remote data source is successful',
-        () async {
-          // Arrange
-          when(() => mockRemoteDataSource.fetchMessages())
-              .thenAnswer((_) async => tEmails);
-          when(() => mockLocalDataSource.cacheMessages(any()))
-              // ignore: avoid_returning_null_for_void
-              .thenAnswer((_) async => null);
-
-          // Act
-          final result = await repository.fetchMessages();
-
-          // Assert
-          verify(() => mockRemoteDataSource.fetchMessages());
-          expect(result, Right(tEmails));
-        },
+    group('getEmails', () {
+      const tSender = 'testSender@test.com';
+      const tSubject = 'testSubject';
+      final tDate = DateTime.now();
+      const tBody = 'testBody';
+      final tEmailModel = MessageModel(
+        sender: tSender,
+        subject: tSubject,
+        date: tDate,
+        body: tBody,
       );
-
+      final tEmails = [tEmailModel];
       test(
-        'should cache the data locally when the call to remote data source is successful',
+        'should check if the device is online',
         () async {
           // Arrange
+          when(() => mockNetworkInfo.isConnected).thenAnswer((_) async => true);
           when(() => mockRemoteDataSource.fetchMessages())
               .thenAnswer((_) async => tEmails);
           when(() => mockLocalDataSource.cacheMessages(any()))
@@ -121,63 +81,102 @@ void main() {
           await repository.fetchMessages();
 
           // Assert
-          verify(() => mockRemoteDataSource.fetchMessages());
-          verify(() => mockLocalDataSource.cacheMessages(tEmails));
+          verify(() => mockNetworkInfo.isConnected);
         },
       );
 
-      test(
-        'should return ServerFailure when the call to remote data source is unsuccessful',
-        () async {
-          // Arrange
-          when(() => mockRemoteDataSource.fetchMessages())
-              .thenThrow(ServerException());
+      runTestsOnline(() {
+        test(
+          'should return remote data when the call to remote data source is successful',
+          () async {
+            // Arrange
+            when(() => mockRemoteDataSource.fetchMessages())
+                .thenAnswer((_) async => tEmails);
+            when(() => mockLocalDataSource.cacheMessages(any()))
+                // ignore: avoid_returning_null_for_void
+                .thenAnswer((_) async => null);
 
-          // Act
-          final result = await repository.fetchMessages();
+            // Act
+            final result = await repository.fetchMessages();
 
-          // Assert
-          verify(() => mockRemoteDataSource.fetchMessages());
-          verifyZeroInteractions(mockLocalDataSource);
-          expect(result, Left(ServerFailure()));
-        },
-      );
-    });
+            // Assert
+            verify(() => mockRemoteDataSource.fetchMessages());
+            expect(result, Right(tEmails));
+          },
+        );
 
-    runTestsOffline(() {
-      test(
-        'should return last locally cached data when the cached data is present',
-        () async {
-          // Arrange
-          when(() => mockLocalDataSource.fetchMessages())
-              .thenAnswer((_) async => tEmails);
+        test(
+          'should cache the data locally when the call to remote data source is successful',
+          () async {
+            // Arrange
+            when(() => mockRemoteDataSource.fetchMessages())
+                .thenAnswer((_) async => tEmails);
+            when(() => mockLocalDataSource.cacheMessages(any()))
+                // ignore: avoid_returning_null_for_void
+                .thenAnswer((_) async => null);
 
-          // Act
-          final result = await repository.fetchMessages();
+            // Act
+            await repository.fetchMessages();
 
-          // Assert
-          verifyZeroInteractions(mockRemoteDataSource);
-          verify(() => mockLocalDataSource.fetchMessages());
-          expect(result, equals(Right(tEmails)));
-        },
-      );
+            // Assert
+            verify(() => mockRemoteDataSource.fetchMessages());
+            verify(() => mockLocalDataSource.cacheMessages(tEmails));
+          },
+        );
 
-      test(
-        'should return CacheFailure data when there is no cached data is present',
-        () async {
-          // Arrange
-          when(() => mockLocalDataSource.fetchMessages())
-              .thenThrow(const CacheException());
+        test(
+          'should return ServerFailure when the call to remote data source is unsuccessful',
+          () async {
+            // Arrange
+            when(() => mockRemoteDataSource.fetchMessages())
+                .thenThrow(ServerException());
 
-          // Act
-          final result = await repository.fetchMessages();
+            // Act
+            final result = await repository.fetchMessages();
 
-          // Assert
-          verifyZeroInteractions(mockRemoteDataSource);
-          verify(() => mockLocalDataSource.fetchMessages());
-          expect(result, Left(CacheFailure()));
-        },
-      );
+            // Assert
+            verify(() => mockRemoteDataSource.fetchMessages());
+            verifyZeroInteractions(mockLocalDataSource);
+            expect(result, Left(ServerFailure()));
+          },
+        );
+      });
+
+      runTestsOffline(() {
+        test(
+          'should return last locally cached data when the cached data is present',
+          () async {
+            // Arrange
+            when(() => mockLocalDataSource.fetchMessages())
+                .thenAnswer((_) async => tEmails);
+
+            // Act
+            final result = await repository.fetchMessages();
+
+            // Assert
+            verifyZeroInteractions(mockRemoteDataSource);
+            verify(() => mockLocalDataSource.fetchMessages());
+            expect(result, equals(Right(tEmails)));
+          },
+        );
+
+        test(
+          'should return CacheFailure data when there is no cached data is present',
+          () async {
+            // Arrange
+            when(() => mockLocalDataSource.fetchMessages())
+                .thenThrow(const CacheException());
+
+            // Act
+            final result = await repository.fetchMessages();
+
+            // Assert
+            verifyZeroInteractions(mockRemoteDataSource);
+            verify(() => mockLocalDataSource.fetchMessages());
+            expect(result, Left(CacheFailure()));
+          },
+        );
+      });
     });
   });
 }

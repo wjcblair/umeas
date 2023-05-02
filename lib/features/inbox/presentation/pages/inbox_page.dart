f961608edaf1ef84a323e21bd9e60a2e0961fefa
William Blair <w.blair@lancaster.ac.uk>
2023-05-02 01:51:23 -0400

    Add blocListener for authBloc loggedInState

--- lib/features/inbox/presentation/pages/inbox_page.dart
+++ lib/features/inbox/presentation/pages/inbox_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:umeas/features/inbox/presentation/bloc/inbox_bloc.dart';
 
 import '../../../../injection_container.dart';
+import '../../../auth/domain/entities/email_provider.dart';
+import '../../../auth/presentation/bloc/auth/auth_bloc.dart';
 
 class InboxPage extends StatelessWidget {
   const InboxPage({super.key});
@@ -12,29 +14,46 @@ class InboxPage extends StatelessWidget {
     return BlocProvider<InboxBloc>(
       create: (context) {
         final inboxBloc = sl<InboxBloc>();
-        inboxBloc.add(const InboxLoadMessagesEvent());
         return inboxBloc;
       },
-      child: BlocBuilder<InboxBloc, InboxState>(
-        builder: (context, state) {
-          if (state is InboxInitialState || state is InboxLoadingState) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (state is InboxLoadedState) {
-            return ListView.builder(
-              itemCount: state.messages.length,
-              itemBuilder: (context, index) {
-                final message = state.messages[index];
-                return ListTile(
-                  title: Text(message.subject),
-                  subtitle: Text(message.sender),
-                );
-              },
-            );
-          } else if (state is InboxErrorState) {
-            return const Center(child: Text('Error loading messages'));
+      child: BlocListener<AuthBloc, AuthState>(
+        listener: (context, state) {
+          if (state is AuthLoggedInState) {
+            if (state.emailProvider == EmailProvider.google) {
+              context
+                  .read<InboxBloc>()
+                  .add(const InboxLoadMessagesWithGoogleApiEvent());
+            } else if (state.emailProvider == EmailProvider.microsoft) {
+              context
+                  .read<InboxBloc>()
+                  .add(const InboxLoadMessagesWithMicrosoftApiEvent());
+            } else {
+              context.read<InboxBloc>().add(const InboxNoMessagesToLoadEvent());
+            }
           }
-          return const SizedBox.shrink();
         },
+        child: BlocBuilder<InboxBloc, InboxState>(
+          builder: (context, state) {
+            if (state is InboxInitialState || state is InboxLoadingState) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is InboxLoadedState) {
+              return ListView.builder(
+                itemCount: state.messages.length,
+                itemBuilder: (context, index) {
+                  final message = state.messages[index];
+                  return ListTile(
+                    title: Text(message.subject),
+                    subtitle: Text(message.sender),
+                  );
+                },
+              );
+            } else if (state is InboxNoMessagesState) {
+            } else if (state is InboxErrorState) {
+              return const Center(child: Text('Error loading messages'));
+            }
+            return const SizedBox.shrink();
+          },
+        ),
       ),
     );
   }

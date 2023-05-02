49b835ef99916f5ab342c2bb05d1e7d16d256fc9
William Blair <w.blair@lancaster.ac.uk>
2023-05-02 01:49:42 -0400

    Update inbox repositories with new providers

--- lib/features/inbox/data/repositories/inbox_repository.dart
+++ lib/features/inbox/data/repositories/inbox_repository.dart
@@ -1,7 +1,9 @@
 import 'package:umeas/core/data/exceptions/server_exception.dart';
 import 'package:umeas/core/network/i_network_info_contract.dart';
+import 'package:umeas/features/auth/domain/entities/email_provider.dart';
 import 'package:umeas/features/inbox/data/datasources/local/i_inbox_local_datasource_contract.dart';
 import 'package:umeas/features/inbox/data/datasources/remote/i_inbox_remote_datasource_contract.dart';
+import 'package:umeas/features/inbox/data/models/message_model.dart';
 import 'package:umeas/features/inbox/domain/entities/message.dart';
 import 'package:umeas/core/domain/failures/app_failure.dart';
 import 'package:dartz/dartz.dart';
@@ -10,37 +12,46 @@ import 'package:umeas/features/inbox/domain/repositories/i_inbox_repository_cont
 import '../../../../core/data/exceptions/cache_exception.dart';
 import '../../../../core/domain/failures/cache_failure.dart';
 import '../../../../core/domain/failures/server_failure.dart';
-import '../datasources/exceptions/inbox_exceptions.dart';
+import '../../domain/failures/inbox_failures.dart';
 
 class InboxRepository implements IInboxRepositoryContract {
   final IInboxLocalDatasourceContract localDatasource;
-  final IInboxRemoteDatasourceContract remoteDatasource;
+  final IInboxRemoteDatasourceContract googleDatasource;
+  final IInboxRemoteDatasourceContract microsoftDatasource;
+
   final INetworkInfoContract networkInfo;
 
   InboxRepository({
     required this.localDatasource,
-    required this.remoteDatasource,
+    required this.googleDatasource,
+    required this.microsoftDatasource,
     required this.networkInfo,
   });
 
   @override
-  Future<Either<AppFailure, List<Message>>> fetchMessages() async {
+  Future<Either<AppFailure, List<Message>>> fetchMessages(
+      EmailProvider provider) async {
     if (await networkInfo.isConnected) {
       try {
-        print('is connected');
-        final messages = await remoteDatasource.fetchMessages();
-        print('messages: $messages');
+        List<MessageModel> messages;
+
+        switch (provider) {
+          case EmailProvider.google:
+            messages = await googleDatasource.fetchMessages();
+            break;
+          case EmailProvider.microsoft:
+            messages = await microsoftDatasource.fetchMessages();
+            break;
+          case EmailProvider.unknown:
+            return Left(UnknownEmailProviderFailure());
+        }
+
         localDatasource.cacheMessages(messages);
         return Right(messages);
-      } on MailingException {
-        return Left(ServerFailure());
-      } on NoInternetConnectionException {
-        return Left(ServerFailure());
-      } on GenericInboxException {
+      } on Exception {
         return Left(ServerFailure());
       }
     } else {
-      print('is not connected');
       try {
         final messages = await localDatasource.fetchMessages();
         return Right(messages);
--- lib/features/inbox/domain/repositories/i_inbox_repository_contract.dart
+++ lib/features/inbox/domain/repositories/i_inbox_repository_contract.dart
@@ -2,7 +2,9 @@ import 'package:dartz/dartz.dart';
 import 'package:umeas/features/inbox/domain/entities/message.dart';
 
 import '../../../../core/domain/failures/app_failure.dart';
+import '../../../auth/domain/entities/email_provider.dart';
 
 abstract class IInboxRepositoryContract {
-  Future<Either<AppFailure, List<Message>>> fetchMessages();
+  Future<Either<AppFailure, List<Message>>> fetchMessages(
+      EmailProvider provider);
 }
